7b6dabdc0e124d8eff20db67269e2038d183eac9
Jyotiprakash Panda <kunup770@gmail.com>
2025-05-18 00:20:45 +0530

    fix: Update Label import in RegisterForm to use named import

--- frontend/src/components/ui/form/RegisterForm.tsx
+++ frontend/src/components/ui/form/RegisterForm.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Input } from "@/components/ui/input";
-import Label from "@/components/ui/label";
+import { Label } from "@/components/ui/label";
 import { IconBrandGoogle } from "@tabler/icons-react";
 import { cn } from "@/lib/utils";
 import { API_BASE_URL } from "@/lib/api";
